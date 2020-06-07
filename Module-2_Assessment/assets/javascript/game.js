348dcca527722514e3e1236ec86b2340f4398dc9
Jeremy Lopez <jlopezcarchi6@gmail.com>
2020-06-07 17:36:01 -0500

    adding functionality for arrays in protoype file

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,6 +1,11 @@
-let answer = "vikings";
+const answers = ['bears', 'broncos', 'cowboys', 'eagles', 'giants', 'packers', 'patriots', 'seahawks', 'texans', 'vikings'];
 
-let curString = createEmptyString(answer.length);
+const images = ['../images/bears.jpg', '../images/broncos.jpg', '../images/cowboys.jpg', '../images/eagles.jpg', '../images/giants.jpg', '../images/packers.jpg', '../images/patriots.jpg', '../images/seahawks.jpg', '../images/texans.jpg', '../images/vikings.jpg'];
+
+let num = Math.ceil(Math.random() * 10) - 1;
+
+let curString = createEmptyString(answers[num].length);
+//"hello".split('').join(' ');
 
 let curAnswer = [];
 
@@ -22,13 +27,13 @@ function createEmptyString(len) {
 document.addEventListener("keyup", function (event) {
   let guess = event.key;
   
-  if(answer.includes(guess) && !curAnswer.includes(guess)) {
-    for(i = 0; i < answer.length; i++) {
-      if(answer[i] === guess) {
+  if(answers[num].includes(guess) && !curAnswer.includes(guess)) {
+    for(i = 0; i < answers[num].length; i++) {
+      if(answers[num][i] === guess) {
         curString = setCharAt(curString, i, guess);
         curAnswer.push(guess);
-        console.log("curString: " +curString);
-        console.log("curAnswer: " +curAnswer);
+        console.log("curString: " + curString.split('').join(' '));
+        //console.log("curAnswer: " + curAnswer);
       }
     }
   }
@@ -39,11 +44,23 @@ document.addEventListener("keyup", function (event) {
   
   if(guessesLeft <= 0) {
     console.log("You Lose!");
-    return;
+    num = Math.ceil(Math.random() * 10) - 1;
+    curString = createEmptyString(answers[num].length);
+    guessesLeft = 13;
+    curAnswer = [];
+    console.log("curString: " + curString.split('').join(' '));
+    //TODO: make a reset function!!!
   }
-  else if(curAnswer.length >= answer.length) {
+  else if(curAnswer.length >= answers[num].length) {
     console.log("You Win!");
-    return;
+    num = Math.ceil(Math.random() * 10) - 1;
+    guessesLeft = 13;
+    curString = createEmptyString(answers[num].length);
+    curAnswer = [];
+    console.log("curString: " + curString.split('').join(' '));
+    //TODO: make a reset function!!!
   }
   
 });
+
+
